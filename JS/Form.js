7e1bdb332a46d73c2cdd9f6abe1ddb30946a1af6
leatherToucan <66318522+leatherToucan@users.noreply.github.com>
2020-10-20 20:03:19 +0530

    Add files via upload

---
+++ JS/Form.js
@@ -0,0 +1,31 @@
+class Form{
+    constructor() {
+
+    }
+
+    display(){
+        var title = createElement('h2')
+        title.html("car racing game");
+        title.position(130, 0);
+
+        var input = createInput("Name");
+        var button = createButton('play');
+
+        input.position(130, 160);
+        button.position(250, 200);
+
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+
+            var name = input.value();
+
+            playerCount = playerCount + 1;
+            p0layer.update(name)
+            player.updateCount(playerCount);
+            var greeting = createElement('h3');
+            greeting.html("hello" + name)
+            greeting.position(130,360)    
+        });
+    }
+}
---
+++ JS/Game.js
@@ -0,0 +1,25 @@
+class game() {
+    constructor(){}
+
+    getState(){
+        var gameStateRef = database.ref('gamestate');
+        gameStateRef.on("value", function(data){
+            gameState = data.val();
+        })
+    }
+
+    update(state){
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+
+    start() {
+        if(gameState === 0){
+            player = new player();
+            player.getCount();
+            form = new Form()
+            form.display();
+        }
+    }
+}
